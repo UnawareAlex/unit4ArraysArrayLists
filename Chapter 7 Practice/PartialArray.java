cb57eb007dd079e864d0e4e0572f0cc1e2b16cb6
UnawareAlex <agarnold@NN123-HA-22.HS.sd203.org>
2015-11-23 14:12:19 -0600

    11/23

--- Chapter 7 Practice/PartialArray.java
+++ Chapter 7 Practice/PartialArray.java
@@ -28,14 +28,44 @@ public class PartialArray
     
     public void insert(int pos, int value)
     {
-        if (this.currentSize < this.values.length)
+        if (this.currentSize == this.values.length)
         {
-            currentSize++;
-            for (int i = this.currentSize - 1; i > pos; i--)
-            {
-                this.values[i] = this.values[i - 1];
-            }
+            this.growArray();
         }
+
+        currentSize++;
+        for (int i = this.currentSize - 1; i > pos; i--)
+        {
+            this.values[i] = this.values[i - 1];
+        }
+        
+        this.values[pos] = value;
     }
+    
+    public void swap(int posA, int posB)
+    {
+        int temp = this.values[posA];
+        this.values[posA] = this.values[posB];
+        this.values[posB] = temp;
     }
+    
+    public void growArray()
+    {
+        int[] newArray = new int[this.currentSize * 2];
+        
+        for (int i = 0; i < this.currentSize; i++)
+        {
+            newArray[i] = this.values[i];
+        }
+        
+        this.values = newArray;
+    }
+    
+    public static void main(String[] args)
+    {
+        PartialArray num = new PartialArray();
+        num.swap(0, 1);
+        num.insert(20, 21);
+    }
+}
 
